4bafb220df9d81b71b14a86a8a9731f639e66656
Cássio Souza <cassiosouza248@gmail.com>
2023-12-18 08:13:43 -0300

    Add project files.

---
+++ ExcelReader/Program.cs
@@ -0,0 +1,53 @@
+﻿using OfficeOpenXml;
+using ReaderAPI;
+
+namespace ExcelReader
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Excel reader! Please put your file path, Example: \"C:/Excels/Archive.xlsx\"  ");
+            string filePath = Console.ReadLine();
+
+            var data = ReadExcel.ReadExcelFile(filePath);
+            var List = ReadExcel.TurnDataIntoList(data);
+
+            int Choice = 0;
+            do
+            {
+                Console.WriteLine("What do you want to do with the values:\n1 - Average\n2 - Biggest Number\n3 - Smallest Number\n4 - Show Numbers\n5 - Sum All\n6 - Show Numbers in grade\n7 - Show First Numbers");
+                Choice = Convert.ToInt32(Console.ReadLine());
+                switch (Choice)
+                {
+                    case 1:
+                        ReadExcel.DoAverage(List);
+                        break;
+                    case 2:
+                        ReadExcel.BiggestNumber(List);
+                        break;
+                    case 3:
+                        ReadExcel.SmallestNumber(List);
+                        break;
+                    case 4:
+                        ReadExcel.ShowNumber(List);
+                        break;
+                    case 5:
+                        ReadExcel.SumNumbers(List);
+                        break;
+                    case 6:
+                        ReadExcel.DisplayData(data);
+                        break;
+                    case 7:
+                        ReadExcel.ShowFirstNumbers(List);
+                        break;
+                    default: Console.WriteLine("Insert a valid operation!");
+                        break;
+                }
+                Console.WriteLine("\t\t");
+            } while (Choice != 99);
+          //  ReadExcel.DisplayData(data);
+        }
+    }
+}
+
---
+++ ReaderAPI/ReadExcel.cs
@@ -0,0 +1,124 @@
+﻿using OfficeOpenXml;
+
+namespace ReaderAPI
+{
+    public class ReadExcel
+    {
+        public static object[,] ReadExcelFile(string filePath)
+        {
+            using (var package = new ExcelPackage(new System.IO.FileInfo(filePath)))
+            {
+                // Get the first worksheet in the Excel file
+                var worksheet = package.Workbook.Worksheets[0];
+
+                // Get the dimensions of the worksheet
+                var start = worksheet.Dimension.Start;
+                var end = worksheet.Dimension.End;
+
+                // Read data into a 2D array
+                object[,] data = new object[end.Row, end.Column];
+
+                for (int row = start.Row; row <= end.Row; row++)
+                {
+                    for (int col = start.Column; col <= end.Column; col++)
+                    {
+                        // Note: EPPlus indexes are 1-based, not 0-based
+                        data[row - 1, col - 1] = worksheet.Cells[row, col].Value;
+                    }
+                }
+
+                return data;
+            }
+        }
+
+        public static List<int> TurnDataIntoList(object[,] data)
+        {
+            List<int> Numbers = new List<int>();
+            for (int row = 0; row < data.GetLength(0); row++)
+            {
+                for (int col = 0; col < data.GetLength(1); col++)
+                {
+                    Numbers.Add(Convert.ToInt32(data[row, col]));
+                }
+            }
+
+            return Numbers;
+        }
+        public static void DoAverage(List<int> Numbers)
+        {
+            double average = 0;
+            double FinalAverage = 0;
+
+            foreach (int Number in Numbers) 
+            {
+                average += Number;
+            }
+
+            FinalAverage = average / Numbers.Count();
+            Console.WriteLine("The Average is: " + FinalAverage);
+        }
+
+        public static void BiggestNumber(List<int> Numbers)
+        {
+            double Biggest = Numbers[0];
+            foreach (int Number in Numbers)
+            {
+                if(Number > Biggest) Biggest = Number;
+            }
+            Console.WriteLine("The Biggest number is: " + Biggest);
+        }
+
+        public static void SmallestNumber(List<int> Numbers)
+        {
+            double Smallest = Numbers[0];
+            foreach (int Number in Numbers)
+            {
+                if (Number < Smallest) Smallest = Number;
+            }
+            Console.WriteLine("The Smallest number is: " + Smallest);
+        }
+
+        public static void ShowNumber(List<int> Numbers)
+        {
+            Console.WriteLine("The numbers are: ");
+            foreach (int Number in Numbers)
+            {
+                Console.WriteLine(Number);
+            }
+        }
+
+        public static void SumNumbers(List<int> Numbers)
+        {
+            double Sum = 0;
+            foreach (int Number in Numbers)
+            {
+                Sum += Number;
+            }
+            Console.WriteLine("The numbers added together are: " + Sum);
+        }
+
+        public static void ShowFirstNumbers(List<int> Numbers)
+        {
+            Console.WriteLine("The first numbers are: ");
+            for(int i = 0; i < 10 && i < Numbers.Count; i++)
+            {
+                Console.WriteLine(Numbers[i]);
+            }
+        }
+
+
+        public static void DisplayData(object[,] data)
+        {
+            // Display data in the console
+            for (int row = 0; row < data.GetLength(0); row++)
+            {
+                Console.WriteLine("The numbers in grade are: ");
+                for (int col = 0; col < data.GetLength(1); col++)
+                {
+                    Console.Write(data[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
